b7d36d1d9f51ddd01702cc59ff626583680fdb93
brnydr <brianwilliamyoder@gmail.com>
2023-08-27 16:56:25 -0700

    add constructor

--- QueenAttack/Models/Queen.cs
+++ QueenAttack/Models/Queen.cs
@@ -1,10 +1,20 @@
 
 
+using System.Data;
+using System.Runtime.CompilerServices;
+
 namespace QueenAttack.Models
 {
   public class Queen
   {
-    
+    public int RankCoord { get; }
+    public int FileCoord { get; }
+  
+  
+    public Queen(int rC, int fC)
+    {
+      RankCoord = rC;
+      FileCoord = fC;
+    }
   }
-
 }
