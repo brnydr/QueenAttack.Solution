f987d8f39586bc59b240f95e59574693a2d0405b
brnydr <brianwilliamyoder@gmail.com>
2023-08-27 16:45:39 -0700

    pass test3 for fileCoordinates

--- QueenAttack.Tests/ModelTests/QueenTests.cs
+++ QueenAttack.Tests/ModelTests/QueenTests.cs
@@ -11,16 +11,24 @@ namespace QueenAttack.Tests
     [TestMethod]
     public void QueenConstructor_CreateInstanceOfQueen_Queen()
     {
-      Queen newQueen = new Queen(4);
+      Queen newQueen = new Queen(4, 7);
       Assert.AreEqual(typeof(Queen), newQueen.GetType());
     }
 
     [TestMethod]
     public void GetRankCoordinate_ReturnRankCoordinate_Int()
     {
-      Queen newQueen = new Queen(4);
+      Queen newQueen = new Queen(4, 7);
       int rank = 4;
       Assert.AreEqual(rank, newQueen.RankCoord);
+    }
+
+    [TestMethod]
+    public void GetFileCoordinate_ReturnFileCoordinate_Int()
+    {
+      Queen newQueen = new Queen(4, 7);
+      int file = 7;
+      Assert.AreEqual(file, newQueen.FileCoord);
     }
   }
 }
